15963a6248ed7833c5042b0916d6e4a10e68ab76
koi <lioncharge1483@gmail.com>
2026-09-06 12:23:02 -0400

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,7 +1,3 @@
---[[ Credits
-    Matas#3535 @matas - Created UI
-    bored#9316 @wally hub user - Helped make library
-]]
 -- // Variables
 local ws = game:GetService("Workspace")
 local uis = game:GetService("UserInputService")
